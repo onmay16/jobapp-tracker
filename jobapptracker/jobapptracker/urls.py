e2ea7f2d0592034da55c69cfa50478edfa1e541e
Sugyeong <onmay16@gmail.com>
2022-06-14 15:08:09 -0500

    backEndProject --> jobapptracker

--- jobapptracker/jobapptracker/urls.py
+++ jobapptracker/jobapptracker/urls.py
@@ -1,4 +1,4 @@
-"""backEndProject URL Configuration
+"""jobapptracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
